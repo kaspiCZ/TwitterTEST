dae1b6ac8b2dbd23ebb0cc7d95c4b383110e67a6
SergiTorres <SergiTorres@users.noreply.github.com>
2017-02-14 12:36:37 +0100

    Add files via upload

---
+++ twitter-api-php-master/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+ini_set('display_errors', 1);
+require_once('TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "token1",
+    'oauth_access_token_secret' => "token2",
+    'consumer_key' => "token3",
+    'consumer_secret' => "token4"
+
+);
+
+$url = 'https://api.twitter.com/1.1/followers/ids.json';
+$getfield = '?screen_name=J7mbo';
+$requestMethod = 'GET';
+
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest(true, array('CURLOPT_SSL_VERIFYHOST' => 0, 'CURLOPT_SSL_VERIFYPEER' => 0));
+    
+?>
